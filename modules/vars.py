2381590ca6d3d8d35a2f300bc78569fceb0d746b
Comrade X <106185020+comrade09@users.noreply.github.com>
2024-09-12 14:39:13 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "28929701"
 api_hash = "ca53fb1dbc7e1d76cdc4d56536d1a5f9"
-bot_token = "6816797780:AAHWOV6Wy7ZCKTV08nOLlekQRrnIS8a_V5M"
+bot_token = "7018626431:AAGztPrWr4KqT8ansAKGLTmHitcb1CdWGWc"
