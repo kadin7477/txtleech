c5fb72b7e803dc88b51a42a1c6da61fe5a0ba501
kadin7477 <kadin489@outlook.com>
2024-09-13 23:26:46 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,7 @@
+import os
 api_id = "28929701"
 api_hash = "ca53fb1dbc7e1d76cdc4d56536d1a5f9"
 bot_token = "7018626431:AAGztPrWr4KqT8ansAKGLTmHitcb1CdWGWc"
+
+WEBHOOK = True  # Don't change this
+PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
